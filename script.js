2bf87bb6b8312422f7ad550651443742fd7e6a31
dannickvaldo <https://email.ionos.de/appsuite/#>
2024-08-21 10:42:35 +0100

    js code

--- script.js
+++ script.js
@@ -50,3 +50,36 @@ configButtons.forEach(button => {
 
 
  
+document.getElementById("registerPrice").addEventListener("submit", async function(event) {
+  event.preventDefault(); 
+  const formData = new FormData();
+  formData.append('name', document.getElementById("name").value);
+  formData.append('number_price', document.getElementById("number_price").value);
+
+
+  // Traitement de l'image
+  const imgInput = document.getElementById("img"); 
+  if (imgInput.files && imgInput.files[0]) {
+    const file = imgInput.files[0];
+    formData.append('img', file, file.name);
+  }
+
+  try {
+    const response = await fetch("http://localhost:4000/price/", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok || response.status === 201) {
+      alert("Votre enregistrement a été réalisé avec succès !");
+    } else {
+      const result = await response.json();
+      alert('Erreur lors de l enregistrement : ${result.error || "Erreur inconnue"}); // Affiche le message d erreur du serveur')
+    }
+  } catch (error) {
+    alert("Une erreur s'est produite pendant l'enregistrement.");
+  }
+});
+
+
+
